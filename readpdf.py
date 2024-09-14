bd30cd10f8df75cb94d55904ece77760d19b2f53
Mehdi Dehbozorgi <113126374+mehdi530@users.noreply.github.com>
2024-09-14 18:32:14 +0330

    Update readpdf.py
    add #discribtion

--- readpdf.py
+++ readpdf.py
@@ -5,17 +5,18 @@ import streamlit as st
 from PyPDF2 import PdfReader
 from streamlit_extras.add_vertical_space import add_vertical_space
 from  langchain.text_splitter import RecursiveCharacterTextSplitter
-
+# sidebar content 
 with st.sidebar:
-    st.title("Hi ")
+    st.title("Hi ") #  set titl
     st.markdown ("""
-    [test link goto google ]("www.google.com")
+    [test link goto google ]("www.google.com") # link example
     
     
     """)
     add_vertical_space(5)
     
 def main():
+# upload and read pdf file    
       st.header("please upload your pdf ...",divider=True)
       pdf=st.file_uploader("upload your pdf",type="pdf")
       if pdf is not None: 
@@ -24,6 +25,7 @@ def main():
        text=""
        for page in pdf_reader.pages:
              text += page.extract_text()
+# set content size for display 
        text_spiliter =  RecursiveCharacterTextSplitter(
             chunk_size=1000 ,
             chunk_overlap=150 ,
